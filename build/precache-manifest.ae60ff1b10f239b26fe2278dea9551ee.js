0c8e83af6c263ac513da7126d9a3f1be4ed157ef
sagnihotri773 <49466830+sagnihotri773@users.noreply.github.com>
2023-03-14 17:25:18 +0530

    Add files via upload

---
+++ build/precache-manifest.ae60ff1b10f239b26fe2278dea9551ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad24975f2592188d68674f7f21585cf6",
+    "url": "/whats_app/index.html"
+  },
+  {
+    "revision": "7475be9f8178bbb27ad9",
+    "url": "/whats_app/static/css/2.0f6a9b5e.chunk.css"
+  },
+  {
+    "revision": "486137a37c7c3da1c996",
+    "url": "/whats_app/static/css/main.cc7fd5dc.chunk.css"
+  },
+  {
+    "revision": "7475be9f8178bbb27ad9",
+    "url": "/whats_app/static/js/2.1e0b5bfb.chunk.js"
+  },
+  {
+    "revision": "4fa8d82e95cd30198be539058cf0df1d",
+    "url": "/whats_app/static/js/2.1e0b5bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486137a37c7c3da1c996",
+    "url": "/whats_app/static/js/main.ebb80b33.chunk.js"
+  },
+  {
+    "revision": "e8680f40eb6683f5977f",
+    "url": "/whats_app/static/js/runtime-main.9d95b94b.js"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/whats_app/static/media/whatsapp-chat-bg.adbccee0.png"
+  },
+  {
+    "revision": "fcf34c63b248034ab94dae263c45e1a9",
+    "url": "/whats_app/static/media/whatsapp-connect.fcf34c63.jpg"
+  }
+]);
